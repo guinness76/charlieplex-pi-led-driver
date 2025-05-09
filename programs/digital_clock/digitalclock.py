a82901b2e8d9582a9d84becc58719d46afaf9fdf
Matt Gross <grossm04@gmail.com>
2025-05-09 15:32:04 -0700

    Finished digital clock

--- programs/digital_clock/digitalclock.py
+++ programs/digital_clock/digitalclock.py
@@ -7,6 +7,7 @@ class DigitalClock(Animation):
     def __init__(self):
         self.name = "digital_clock"
         self.current_time = "0000"
+        self.seconds_brightness = 10
 
         characters_file = "../resources/small-numbers.json"
         with open(characters_file, 'r') as the_file:
@@ -17,22 +18,17 @@ class DigitalClock(Animation):
         the_time = now.strftime("%H%M")
         seconds = now.strftime("%S")
 
-        # Draw the seconds dots on the bottom
+        # Clear the seconds area on the bottom
         piutils.fill_area(display, 0, 7, 15, 8, 0)
         indicator = int(seconds)
-        
-        if indicator <= 15:
-            piutils.draw_line(display, 0, 7, indicator, 7, 25)
-        elif indicator > 15 and indicator <= 30:
-            piutils.draw_line(display, 0, 7, 15, 7, 25)
-            piutils.draw_line(display, 0, 8, indicator-15, 8, 25)
-        elif indicator > 30 and indicator <= 45:
-            piutils.draw_line(display, indicator-30, 7, 15, 7, 25)
-            piutils.draw_line(display, 0, 8, 15, 8, 25)
-        else:
-            piutils.draw_line(display, indicator-45, 8, 15, 8, 25)
-        
 
+        if indicator == 0:
+            pass
+        elif indicator >= 1 and indicator <= 30:
+            self.render_seconds_increment(display, indicator)
+        elif indicator > 30:
+            self.render_seconds_decrement(display, indicator)
+            
         # Don't update the numbers if hours and minutes did not change
         if self.current_time == the_time:
             return False
@@ -60,10 +56,29 @@ class DigitalClock(Animation):
                 acc_x = acc_x + 4
 
         return False
+
+    def render_seconds_increment(self, display, indicator):
+        if indicator >= 1 and indicator <= 15:
+            x = indicator-1
+            piutils.draw_line(display, 0, 7, x, 7, self.seconds_brightness)
+        else:
+            x = indicator-16
+            piutils.draw_line(display, 0, 7, 14, 7, self.seconds_brightness)
+            piutils.draw_line(display, 0, 8, x, 8, self.seconds_brightness)    
+
+    def render_seconds_decrement(self, display, indicator):
+        if indicator >= 31 and indicator <= 44:
+            # '15-' inverts the line
+            x = 15-(45-indicator)
+            piutils.draw_line(display, x, 7, 14, 7, self.seconds_brightness)
+            piutils.draw_line(display, 0, 8, 14, 8, self.seconds_brightness)
+        else:
+            # '15-' inverts the line
+            x = 15-(60-indicator)
+            piutils.draw_line(display, x, 8, 14, 8, self.seconds_brightness)
     
     def render_current_time(self):
         sprites = []
-        # sprite_rows = []    
 
         for the_char in self.current_time:
             if not the_char in self.character_map:
@@ -71,21 +86,5 @@ class DigitalClock(Animation):
 
             the_sprite = self.character_map[the_char]
             sprites.append(the_sprite)
-            # sprite_rows.append(the_sprite)
 
         return sprites
-
-        # Each row from all the sprites must be stitched together.
-        # for i in range(0, 9):
-        #     current_row = 0
-        #     full_row = []
-        #     for sprite in sprite_rows:
-        #         sprite_row = sprite[i]
-
-        #         for j in range(0, len(sprite_row)):
-        #             full_row.append(sprite_row[j])
-
-        #     current_row = current_row + 1
-        #     sprites.append(full_row)
-
-        # return sprites
